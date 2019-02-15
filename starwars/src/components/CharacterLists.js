81e6b1df6303b7b749544d8bb01586a6f2d665c3
lilvina <davinaleo2007@yahoo.com>
2019-02-15 10:06:54 -0800

    finished MVP

---
+++ starwars/src/components/CharacterLists.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CharacterLists = props => {
+  return (
+    <div className="starwars-list-container">
+      {props.characters.map(character => {
+        return (
+          <div className={`starwar-list ${character.name}`} key={character.name}>
+            <h3>{character.name}</h3>
+            <p><strong>Born:</strong>{character.born}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default CharacterLists;
